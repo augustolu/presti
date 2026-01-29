022078fa269df0f983a57660933980d4f12d7604
augustolu <augustoluceropollio@gmail.com>
2026-01-29 01:38:18 -0300

    config

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/presti",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "via.placeholder.com",
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/assets/:all*(mp4|jpg|png|webp|svg)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
   },
 };
 
